2434015a8533d462b28fbfe3a37966ec636752b2
Corey Light <coreylight@gmail.com>
2015-08-26 14:56:10 -0500

    circle needs to run prod not buildwatch

--- gulp/tasks/docker.js
+++ gulp/tasks/docker.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var config = require('../config').html;
 var shell = require('gulp-shell');
 
-gulp.task('docker', ['build'], function() {
+gulp.task('docker', ['prod'], function() {
   return gulp.src('/', {read:false}).pipe(shell([
     'docker build -t quay.io/opsee/emissary .'
     ]))
