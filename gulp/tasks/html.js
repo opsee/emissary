1952e98486303d9c1cdff16a30982361511ac3b8
Corey Light <coreylight@gmail.com>
2015-08-27 10:14:47 -0500

    better prod build

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,6 +3,8 @@ var config = require('../config').html;
 var preprocess = require('gulp-preprocess');
 var argv = require('yargs').argv;
 
+console.log('Preprocessing HTML as '+argv.env || 'development');
+
 gulp.task('html', function() {
   return gulp.src(config.src)
   .pipe(preprocess({context: { ENV: argv.env}}))
