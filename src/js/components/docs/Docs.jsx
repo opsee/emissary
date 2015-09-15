dd99f0fda32c33e99f1e70ff409fc2648f315190
Stephen Boak <stephen.boak@gmail.com>
2015-09-14 19:52:37 -0700

    docs

---
+++ src/js/components/docs/Docs.jsx
@@ -0,0 +1,26 @@
+import React, {PropTypes} from 'react';
+import {Button} from '../../modules/bootstrap';
+import {Toolbar} from '../global';
+import {Link} from 'react-router';
+import {Grid, Row, Col} from '../../modules/bootstrap';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <Toolbar title="Opsee Documentation"/>
+        <Grid>
+          <Row>
+            <Col xs={12} sm={10} smOffset={1}>
+              <h3>RTFM</h3>
+              <ul>
+                <li><Link to="docsBastion">The Bastion Instance</Link></li>
+                <li><Link to="docsCloudformation">Our Cloudformation Template and Permissions</Link></li>
+              </ul>
+              </Col>
+            </Row>
+          </Grid>
+      </div>
+    );
+  }
+});
