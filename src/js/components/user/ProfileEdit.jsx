1190fe0a6359fcc3dfb74fb4ad03305e1a38475a
Corey Light <coreylight@gmail.com>
2015-09-15 11:37:59 -0500

    profile edit stuff

--- src/js/components/user/ProfileEdit.jsx
+++ src/js/components/user/ProfileEdit.jsx
@@ -3,31 +3,24 @@ import {UserStore} from '../../stores';
 import {UserActions} from '../../actions';
 import {Toolbar} from '../global';
 import {Link} from 'react-router';
-import UserInputs from '../user/UserInputs.jsx';
 import _ from 'lodash';
 import router from '../../modules/router.js';
 import {PageAuth} from '../../modules/statics';
 import {Grid, Row, Col} from '../../modules/bootstrap';
 import forms from 'newforms';
 import {Button, BoundField} from '../forms';
+import UserInputs from './UserInputs.jsx';
 
-const InfoForm = forms.Form.extend({
-  email: forms.CharField({
-    widgetAttrs:{
-      placeholder:'email'
-    }
-  }),
-  name: forms.CharField({
-    widgetAttrs:{
-      placeholder:'name'
-    }
-  }),
+const PasswordForm = forms.Form.extend({
   password: forms.CharField({
     widget: forms.PasswordInput,
     widgetAttrs:{
       placeholder:'password'
     }
-  })
+  }),
+  render(){
+    return <BoundField bf={this.boundField('password')}/>
+  }
 });
 
 function getData(){
@@ -40,38 +33,53 @@ export default React.createClass({
     willTransitionTo:PageAuth
   },
   storeDidChange(){
-    // const status = UserStore.
-    this.setState({
-      user:UserStore.getUser().toJS()
-    });
+    const status = UserStore.getUserGetUserStatus();
+    if(status == 'success'){
+      this.setState({
+        user:UserStore.getUser().toJS(),
+        password:this.getForm()
+      });
+    }
   },
   getInitialState() {
-    var self = this;
+    // return this.getForm();
     return {
-      info:new InfoForm(_.extend({
-        onChange(){
-          self.props.onChange(self.state.info.cleanedData);
-          self.forceUpdate();
-        },
-        labelSuffix:'',
-        validation:{
-          on:'blur change',
-          onChangeDelay:100
-        },
-      },self.dataComplete()))
+      user:UserStore.getUser().toJS(),
+      password:this.getForm()
     }
+  },
+  getForm(){
+    var self = this;
+    return new PasswordForm({
+      onChange(){
+        self.props.onChange(self.state.info.cleanedData);
+        self.forceUpdate();
+      },
+      labelSuffix:'',
+      validation:{
+        on:'blur change',
+        onChangeDelay:100
+      },
+    });
+  },
+  updateUserData(data){
+    this.setState({
+      info:data
+    })
+  },
+  updatePassword(password){
+    this.setState({password})
   },
   dataComplete(){
-    UserStore.getUser()
-    const test = _.chain(this.props.include).map(s => this.props[s]).every().value();
-    if(test){
-      return {
-        data:this.props
-      }
-    }
+    return UserStore.getUser().get('name') && UserStore.getUser().toJS();
   },
   componentWillMount(){
-    UserActions.userGetUser(UserStore.getUser().get('id'));
+    if(!this.dataComplete()){
+      UserActions.userGetUser(UserStore.getUser().get('id'));
+    }
+  },
+  submit(){
+
   },
   render() {
     return (
@@ -80,9 +88,11 @@ export default React.createClass({
         <Grid>
           <Row>
             <Col xs={12} sm={10} smOffset={1}>
-              <BoundField bf={this.state.info.boundField('name')}/>
-              <BoundField bf={this.state.info.boundField('email')}/>
-              <BoundField bf={this.state.info.boundField('password')}/>
+            <form onSubmit={this.submit}>
+              <UserInputs include={['email', 'name']}  onChange={this.updateUserData} email={this.state.user.email} name={this.state.user.name}/>
+              {this.state.password.render()}
+              <Button bsStyle="primary" type="submit">Submit</Button>
+            </form>
             </Col>
           </Row>
         </Grid>
