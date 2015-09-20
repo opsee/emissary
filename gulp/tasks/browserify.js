7cc8c49976b11319e6f01a8435c65178e297359a
Corey Light <coreylight@gmail.com>
2015-09-20 17:06:31 -0500

    fix browserify watching and errors

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -6,32 +6,53 @@ var watchify = require('watchify');
 var connect = require('gulp-connect');
 var config = require('../config').browserify;
 var transform = require('vinyl-transform');
-
-watchify.args.debug = config.debug;
-//needed for discify
-// watchify.args.fullPaths = true;
-var bundler = watchify(browserify(config.src, watchify.args), {poll:true});
-config.transform.forEach(function(t) {
-  bundler.transform(t);
-});
-
-gulp.task('browserifyWatch', bundle);
-bundler.on('update', bundle);
+var notify = require("gulp-notify");
 
 gulp.task('browserify', function(){
-  return browserify(config.src, config).bundle()
-  .pipe(source('index.js'))
-  .pipe(gulp.dest(config.dest))
+  return buildScript();
+  return buildScript(config.src);
 });
 
-function bundle() {
-  return bundler.bundle()
-  // log errors if they happen
-  .on('error', function(err){
-    gutil.log(err.toString(), 'Browserify Error')
-    this.emit('end');
-  })
-  .pipe(source(config.outputName))
-  .pipe(gulp.dest(config.dest))
-  .pipe(connect.reload());
+gulp.task('browserifyWatch', function(){
+  return buildScript(true);
+});
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+function buildScript(watch) {
+  var props = {
+    entries: [config.src], 
+    paths: ['./src/js/', './node_modules'],
+    src: config.src,
+    dest: config.dest,
+    transform:['babelify', 'reactify'],
+    outputName:'index.js'
+  };
+  // var bundler = browserify(config.src, props);
+  var bundler = browserify(config.src, watchify.args);
+  bundler = watch ? watchify(bundler) : bundler;
+  config.transform.forEach(function(t) {
+    bundler.transform(t);
+  });
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream.on('error', handleErrors)
+    .pipe(source('index.js'))
+    .pipe(gulp.dest(config.dest))
+  }
+
+  if(watch){
+    bundler.on('update', function() {
+      rebundle();
+      gutil.log('Rebundle...');
+    });
+  }
+  return rebundle();
 }
