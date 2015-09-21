14cb5aad7bcddcd7d7aa5d930fd515facb6c9b48
Mark Martin <assplecake@gmail.com>
2015-09-21 15:28:36 -0700

    try the latest tag

--- gulp/tasks/docker.js
+++ gulp/tasks/docker.js
@@ -4,6 +4,6 @@ var shell = require('gulp-shell');
 
 gulp.task('docker', ['prod'], function() {
   return gulp.src('/', {read:false}).pipe(shell([
-    'docker build -t quay.io/opsee/emissary .'
+    'docker build -t quay.io/opsee/emissary:latest .'
     ]))
 });
