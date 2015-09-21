812567954f9cafba43c5071d8f6d9bc72c32dd25
Corey Light <coreylight@gmail.com>
2015-09-21 18:41:13 -0500

    fucking

--- gulp/tasks/docker.js
+++ gulp/tasks/docker.js
@@ -2,12 +2,8 @@ var gulp = require('gulp');
 var config = require('../config').html;
 var shell = require('gulp-shell');
 
-gulp.task('docker', ['prod'], function() {
+gulp.task('docker', function() {
   return gulp.src('/', {read:false}).pipe(shell([
     'docker build -t quay.io/opsee/emissary:latest .'
     ]))
-});
-
-gulp.task('test', function() {
-  console.log('TEST HEY');
 });
