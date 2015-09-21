bed550b94c689dfffa4a427e0d3de74e7d3ddb8f
Corey Light <coreylight@gmail.com>
2015-09-21 17:56:16 -0500

    build stuff

--- gulp/tasks/prod.js
+++ gulp/tasks/prod.js
@@ -1,2 +1,4 @@
 var gulp = require('gulp');
-gulp.task('prod', ['uglify', 'html', 'styles', 'buildImg', 'buildFiles']);
+gulp.task('prod', ['uglify', 'html', 'styles', 'buildImg'], function(){
+  gulp.start('buildFiles');
+});
