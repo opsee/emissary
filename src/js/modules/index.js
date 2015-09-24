fcffd06963697ff1e7236cfd858d3fcf5cb7e9cb
Corey Light <coreylight@gmail.com>
2015-09-24 15:20:44 -0500

    fix things I broke

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,11 +1,11 @@
-import bootstrap from './boostrap';
+import bootstrap from './bootstrap';
 import config from './config';
 import Flux from './flux';
 import ga from './ga';
 import mixins from './mixins';
 import request from './request';
 import router from './router';
-import statics form './statics';
+import statics from './statics';
 import storage from './storage';
 import variables from './variables';
 
