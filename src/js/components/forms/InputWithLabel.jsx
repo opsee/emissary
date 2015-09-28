345212fd1ae96556f5a6b74cf1e30acdd8a227a8
Stephen Boak <stephen.boak@gmail.com>
2015-09-28 10:55:04 -0700

    input styles

--- src/js/components/forms/InputWithLabel.jsx
+++ src/js/components/forms/InputWithLabel.jsx
@@ -4,20 +4,9 @@ import Label from './Label.jsx';
 export default React.createClass({
   render(){
     return(
-      <div>
-        <Label bf={this.props.bf}>
-          {this.props.children}
-        </Label>
-        <div className="display-flex flex-vertical-align">
-          {
-          // <label htmlFor={this.props.bf.idForLabel()}>
-          //   {this.props.children}
-          // </label>
-          }
-          <div className="flex-1">
-            {this.props.bf.render()}
-          </div>
-        </div>
+      <div className="display-flex flex-column">
+        <div className="flex-order-2">{this.props.bf.render()}</div>
+        <Label className="flex-order-1" bf={this.props.bf}/>
       </div>
     )
   }
