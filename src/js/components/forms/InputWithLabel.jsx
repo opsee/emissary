32cb6d83fe26752bb9feecdc9c285c105d870f57
Corey Light <coreylight@gmail.com>
2015-10-13 10:55:57 -0500

    rework inputwithlabel a tad

--- src/js/components/forms/InputWithLabel.jsx
+++ src/js/components/forms/InputWithLabel.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 import Label from './Label.jsx';
 
 export default React.createClass({
+  renderLabel(){
+    if(this.props.bf.field.widgetAttrs.noLabel){
+      return <span/>
+    }else{
+      <Label className="flex-order-1" bf={this.props.bf} />
+    }
+  },
   render(){
     var classString = 'flex-column';
     if (this.props.children) {
       classString += ' has-icon';
     }
-    if (this.props.bf.field.widgetAttrs.noLabel) {
-      return (
-        <div className={classString}>
-          <div className="input-container flex-order-2">{this.props.bf.render()}</div>
-          {this.props.children}
-        </div>
-      )
-    } else {
-      return (
-        <div className={classString}>
-          <div className="input-container flex-order-2">{this.props.bf.render()}</div>
-            <Label className="flex-order-1" bf={this.props.bf} />
-            {this.props.children}
-        </div>
-      )
-    }
+    return (
+      <div className={classString}>
+        <div className="input-container flex-order-2">{this.props.bf.render()}</div>
+        {this.renderLabel()}
+        {this.props.children}
+      </div>
+    )
   }
 });
