1ed350bc68e7b55cb576214cef9ea399818a213d
Corey Light <coreylight@gmail.com>
2015-10-15 18:32:59 -0500

    tweaks

--- test-deploy.js
+++ test-deploy.js
@@ -1,7 +1,7 @@
 casper.options.verbose = true;
 casper.options.pageSettings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36";
 casper.test.begin('Ensure proper build', 1, function(test){
-  casper.start('http://localhost:8080/login', function(){
+  casper.start('http://localhost:9000/login', function(){
     casper.waitForSelector('h1', function pass(){
       test.pass('Found h1');
     },
@@ -13,23 +13,3 @@ casper.test.begin('Ensure proper build', 1, function(test){
     test.done();
   });
 });
-
-// var casper = require('casper').create();
-// casper.on('remote.message', function(msg){
-//   casper.echo('From browser: '+msg);
-// })
-// casper.start();
-// debugger;
-// casper.thenOpen('http://localhost:8080', function(){
-//   casper.waitForSelector('h1', function(){
-//     var text = this.evaluate(function(){
-//       return document.querySelector('h1').textContent;
-//     })
-//     this.echo(text);
-//   }, null, 15000)
-// });
-// casper.run(function(){
-//   this.exit();
-// });
-
-// var text = __utils__.findOne('h1').textContent.toLowerCase();
