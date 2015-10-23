a28f84b0065ae0ebb62e7799b031299825651899
Corey Light <coreylight@gmail.com>
2015-10-23 09:12:17 -0500

    add create check to group single pages

--- src/js/components/groups/GroupSecurity.jsx
+++ src/js/components/groups/GroupSecurity.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Immutable from 'immutable';
+import TimeAgo from 'react-components/timeago';
+
 import {StatusHandler, Table, Toolbar} from '../global';
 import {CheckItemList} from '../checks';
 import {InstanceItemList} from '../instances';
-import TimeAgo from 'react-components/timeago';
 import GroupItem from './GroupItem.jsx';
 import {GroupStore, InstanceStore} from '../../stores';
 import {GroupActions, CheckActions, InstanceActions} from '../../actions';
 import {SetInterval} from '../../modules/mixins';
-import Immutable from 'immutable';
+import {Button} from '../forms';
 import {Grid, Row, Col} from '../../modules/bootstrap';
+import {Add} from '../icons';
+import {Padding} from '../layout';
 
 export default React.createClass({
   mixins: [GroupStore.mixin, SetInterval],
@@ -89,6 +93,11 @@ export default React.createClass({
                 <div className="padding-b">
                   <h3>Checks</h3>
                   <CheckItemList type="groupSecurity" id={this.props.params.id}></CheckItemList>
+                  <Padding t={2}>
+                    <Button bsStyle="primary" className="text-left" to="checkCreateRequest" query={{target:{id:this.state.group.get('id'), type:'security'}}}>
+                      <Add inline={true}/> Create Check
+                    </Button>
+                  </Padding>
                 </div>
               </Col>
             </Row>
