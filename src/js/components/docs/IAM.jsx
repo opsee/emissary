9593743007dc8b97471c13776ad99446e930b8d7
Stephen Boak <stephen.boak@gmail.com>
2015-11-09 16:13:58 -0800

    minor tweaks

--- src/js/components/docs/IAM.jsx
+++ src/js/components/docs/IAM.jsx
@@ -40,6 +40,7 @@ export default React.createClass({
                 </ol>
                 <Highlight>
                 {JSON.stringify({
+                  'Version': '2012-10-17',
                   'Statement': [
                     {
                       'Effect': 'Allow',
