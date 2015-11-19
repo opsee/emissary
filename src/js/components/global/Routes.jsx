93ace7095a3f4ccec9ea95a539ab99c9cea6a75d
Corey Light <coreylight@gmail.com>
2015-11-19 13:27:31 -0600

    start

--- src/js/components/global/Routes.jsx
+++ src/js/components/global/Routes.jsx
@@ -59,69 +59,69 @@ import NotFound from '../pages/NotFound.jsx';
 import System from '../env/System.jsx';
 
 const routes = (
-  <Route handler={Opsee}>
-    <Route path="styleguide" name="styleguide" handler={Styleguide}/>
+  <Route component={Opsee}>
+    <Route path="styleguide" component={Styleguide}/>
 
-    <Route path="/env" name="env" handler={Env}/>
-    <Route path="/env-groups-security" name="envGroupsSecurity" handler={EnvGroupsSecurity}/>
-    <Route path="/env-groups-elb" name="envGroupsELB" handler={EnvGroupsELB}/>
-    <Route path="/env-instances-ec2" name="envInstancesEC2" handler={EnvInstancesEC2}/>
-    <Route path="/instance/ec2/:id" name="instanceEC2" handler={InstanceEC2}/>
-    <Route path="/instance/rds/:id" name="instanceRDS" handler={InstanceRDS}/>
+    <Route path="/env" component={Env}/>
+    <Route path="/env-groups-security" component={EnvGroupsSecurity}/>
+    <Route path="/env-groups-elb" component={EnvGroupsELB}/>
+    <Route path="/env-instances-ec2" component={EnvInstancesEC2}/>
+    <Route path="/instance/ec2/:id" component={InstanceEC2}/>
+    <Route path="/instance/rds/:id" component={InstanceRDS}/>
 
-    <Route path="/" name="checks" handler={CheckList}/>
+    <Route path="/" component={CheckList}/>
     <Redirect from="/checks" to="/" />
 
-    <Route path="/check-create" name="checkCreate" handler={CheckCreate}>
+    <Route path="/check-create" component={CheckCreate}>
       <Redirect from="/check-create" to="/check-create/target" />
-      <Route path="/check-create/target" name="checkCreateTarget" handler={CheckCreateTarget}/>
-      <Route path="/check-create/request" name="checkCreateRequest" handler={CheckCreateRequest}/>
-      <Route path="/check-create/assertions" name="checkCreateAssertions" handler={CheckCreateAssertions}/>
-      <Route path="/check-create/info" name="checkCreateInfo" handler={CheckCreateInfo}/>
+      <Route path="/check-create/target" component={CheckCreateTarget}/>
+      <Route path="/check-create/request" component={CheckCreateRequest}/>
+      <Route path="/check-create/assertions" component={CheckCreateAssertions}/>
+      <Route path="/check-create/info" component={CheckCreateInfo}/>
     </Route>
-    <Route path="/check/edit/:id" name="checkEdit" handler={CheckEdit}/>
-    <Route path="/check/:id" name="check" handler={CheckSingle}>
-      <NotFoundRoute handler={CheckNotFound}/>
+    <Route path="/check/edit/:id" component={CheckEdit}/>
+    <Route path="/check/:id" component={CheckSingle}>
+      <NotFoundRoute component={CheckNotFound}/>
     </Route>
 
-    <Route path="/group/security/:id" name="groupSecurity" handler={GroupSecurity}/>
-    <Route path="/group/elb/:id" name="groupELB" handler={GroupELB}/>
+    <Route path="/group/security/:id" component={GroupSecurity}/>
+    <Route path="/group/elb/:id" component={GroupELB}/>
 
-    <Route path="/start" name="start" handler={OnboardCreate}/>
-    <Route path="/start/thanks" name="onboardThanks" handler={OnboardThanks}/>
-    <Route path="/start/password" name="onboardPassword" handler={OnboardPassword}/>
-    <Route path="/start/tutorial" name="tutorial" handler={OnboardTutorial}>
+    <Route path="/start" component={OnboardCreate}/>
+    <Route path="/start/thanks" component={OnboardThanks}/>
+    <Route path="/start/password" component={OnboardPassword}/>
+    <Route path="/start/tutorial" component={OnboardTutorial}>
       <Redirect from="/start/tutorial" to="/start/tutorial/1" />
-      <Route path="/start/tutorial/1" name="tutorial1" handler={OnboardTutorial1}/>
-      <Route path="/start/tutorial/2" name="tutorial2" handler={OnboardTutorial2}/>
-      <Route path="/start/tutorial/3" name="tutorial3" handler={OnboardTutorial3}/>
+      <Route path="/start/tutorial/1" component={OnboardTutorial1}/>
+      <Route path="/start/tutorial/2" component={OnboardTutorial2}/>
+      <Route path="/start/tutorial/3" component={OnboardTutorial3}/>
     </Route>
-    <Route path="/start/profile" name="onboardProfile" handler={Help}/>
-    <Route path="/start/team" name="onboardTeam" handler={OnboardTeam}/>
-    <Route path="/start/region-select" name="onboardRegionSelect" handler={OnboardRegionSelect}/>
-    <Route path="/start/credentials" name="onboardCredentials" handler={OnboardCredentials}/>
-    <Route path="/start/vpc-select" name="onboardVpcSelect" handler={OnboardVPCSelect}/>
-    <Route path="/start/install" name="onboardInstall" handler={OnboardInstall}/>
-    <Route path="/start/install-example" name="onboardInstallExample" handler={OnboardInstall} example/>
+    <Route path="/start/profile" component={Help}/>
+    <Route path="/start/team" component={OnboardTeam}/>
+    <Route path="/start/region-select" component={OnboardRegionSelect}/>
+    <Route path="/start/credentials" component={OnboardCredentials}/>
+    <Route path="/start/vpc-select" component={OnboardVPCSelect}/>
+    <Route path="/start/install" component={OnboardInstall}/>
+    <Route path="/start/install-example" component={OnboardInstall} example/>
 
-    <Route path="/admin/signups" name="adminSignups" handler={AdminSignups}/>
-    <Route path="/admin/socket" name="adminSocket" handler={AdminSocket}/>
-    <Route path="/system" name="system" handler={System}/>
+    <Route path="/admin/signups" component={AdminSignups}/>
+    <Route path="/admin/socket" component={AdminSocket}/>
+    <Route path="/system" component={System}/>
 
-    <Route path="/profile" name="profile" handler={Profile}/>
-    <Route path="/profile/edit" name="profileEdit" handler={ProfileEdit}/>
-    <Route path="/login" name="login" handler={Login}/>
-    <Route path="/password-forgot" name="passwordForgot" handler={PasswordForgot}/>
-    <Route path="/password-change" name="passwordChange" handler={PasswordChange}/>
+    <Route path="/profile" component={Profile}/>
+    <Route path="/profile/edit" component={ProfileEdit}/>
+    <Route path="/login" component={Login}/>
+    <Route path="/password-forgot" component={PasswordForgot}/>
+    <Route path="/password-change" component={PasswordChange}/>
 
-    <Route path="/help" name="help" handler={Help}/>
+    <Route path="/help" component={Help}/>
 
-    <Route path="/docs" name="docs" handler={Docs}/>
-    <Route path="/docs/bastion" name="docsBastion" handler={DocsBastion}/>
-    <Route path="/docs/cloudformation" name="docsCloudformation" handler={DocsCloudformation}/>
-    <Route path="/docs/IAM" name="docsIAM" handler={DocsIAM}/>
+    <Route path="/docs" component={Docs}/>
+    <Route path="/docs/bastion" component={DocsBastion}/>
+    <Route path="/docs/cloudformation" component={DocsCloudformation}/>
+    <Route path="/docs/IAM" component={DocsIAM}/>
 
-    <NotFoundRoute handler={NotFound}/>
+    <NotFoundRoute component={NotFound}/>
   </Route>
 );
 
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Perf from 'react-addons-perf';
-import router from './modules/router.js';
+// import router from './modules/router.js';
 import _ from 'lodash';
 import {hideNavList} from './components/global/Routes.jsx';
 import {GlobalActions} from './actions';
 import config from './modules/config';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {routes} from './components/global/Routes.jsx';
+import ReactRouter, {Router, Route} from 'react-router';
 
 if (config.env !== 'production'){
   window._ = _;
@@ -13,23 +15,27 @@ if (config.env !== 'production'){
   window.Perf.start();
 }
 
-router.run((Root, state) => {
-  const testRoutes = state.routes;
-  const names = _.pluck(testRoutes, 'name');
-  const shouldHideNav = !(_.intersection(names, hideNavList).length);
-  GlobalActions.globalSetNav(shouldHideNav);
-  ReactDOM.render(
-      (
-        <div>
-          <Root params={state.params} {...state}>
-          {
-            // <CSSTransitionGroup transitionName="example">
-            //   {React.cloneElement(this.props.children || <div/>, {key: 'foo'})}
-            // </CSSTransitionGroup>
-          }
-          </Root>
-        </div>
-      ),
-      document.getElementById('main')
-  );
-});
+render(
+  <Router>{routes}</Router>, document.getElementById('main')
+);
+
+// router.run((Root, state) => {
+//   const testRoutes = state.routes;
+//   const names = _.pluck(testRoutes, 'name');
+//   const shouldHideNav = !(_.intersection(names, hideNavList).length);
+//   GlobalActions.globalSetNav(shouldHideNav);
+//   render(
+//       (
+//         <div>
+//           <Root params={state.params} {...state}>
+//           {
+//             // <CSSTransitionGroup transitionName="example">
+//             //   {React.cloneElement(this.props.children || <div/>, {key: 'foo'})}
+//             // </CSSTransitionGroup>
+//           }
+//           </Root>
+//         </div>
+//       ),
+//       document.getElementById('main')
+//   );
+// });
