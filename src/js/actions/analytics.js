40672e9c1b25b75c75713b5450ffef68511d6d1c
Sara Bee <sara.bainbridge@gmail.com>
2016-01-20 11:06:35 -0500

    Add analytics trackEvent action

--- src/js/actions/analytics.js
+++ src/js/actions/analytics.js
@@ -1,7 +1,7 @@
 import config from '../modules/config';
 import request from '../modules/request';
 import URL from 'url';
-import {ANALYTICS_EVENT} from './constants';
+import {ANALYTICS_EVENT, ANALYTICS_PAGEVIEW} from './constants';
 
 const ANALYTICS_CONFIG = config.services.analytics;
 const ANALYTICS_API = URL.format(ANALYTICS_CONFIG);
@@ -24,10 +24,33 @@ export function trackPageView(path, title) {
     const user = makeUserObject(state().user);
 
     dispatch({
-      type: ANALYTICS_EVENT,
+      type: ANALYTICS_PAGEVIEW,
       payload: request
         .post(`${ANALYTICS_API}/pageview`)
         .send({ path, name, user })
+    });
+  };
+}
+
+export function trackEvent(category, action = '', data = {}) {
+  return (dispatch, state) => {
+    if (config.ghosting){
+      return Promise.resolve();
+    }
+
+    if (!category){
+      if (config.env !== 'production'){
+        console.warn('No category supplied to analytics event');
+      }
+      return Promise.resolve();
+    }
+
+    const user = makeUserObject(state().user);
+    dispatch({
+      type: ANALYTICS_EVENT,
+      payload: request
+      .post(`${ANALYTICS_API}/event`)
+      .send({ category, action, user, data })
     });
   };
 }
