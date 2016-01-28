9e8a938a2babb9ff963c04bf4695081f7379beb2
Corey Light <coreylight@gmail.com>
2016-01-28 17:54:14 -0600

    allow for remotedebugport in config for weinre use

--- src/js/components/global/App.jsx
+++ src/js/components/global/App.jsx
@@ -8,6 +8,11 @@ import routes from './Routes.jsx';
 
 if (config.env !== 'production'){
   window._ = _;
+  if (config.remoteDebugPort){
+    /*eslint-disable*/
+    (function(e){e.setAttribute("src",`http://${window.location.hostname}:${config.remoteDebugPort}/target/target-script-min.js#anonymous`);document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"));void(0);
+    /*eslint-enable*/
+  }
   // window.Perf = require('react-addons-perf');
   // window.Perf.start();
 }
