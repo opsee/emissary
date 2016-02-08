6edc294a7000a325fc8bc07635c79b45ad62e4cf
Sara Bee <sara.bainbridge@gmail.com>
2016-02-08 15:09:41 -0500

    Tidying up the Screenshot component :bath:

--- src/js/components/checks/Screenshot.jsx
+++ src/js/components/checks/Screenshot.jsx
@@ -5,12 +5,7 @@ import {Map} from 'immutable';
 
 import {checks as actions} from '../../actions';
 import {Grid, Row, Col} from '../../modules/bootstrap';
-import {Padding} from '../layout';
-import {Heading} from '../type';
 import {Toolbar} from '../global';
-import AssertionItemList from './AssertionItemList';
-import NotificationItemList from './NotificationItemList';
-import HttpRequestItem from './HttpRequestItem';
 import CheckResponsePaginate from './CheckResponsePaginate';
 
 /*eslint-disable no-unused-vars*/
@@ -34,61 +29,42 @@ const CheckScreenshot = React.createClass({
     })
   },
   componentWillMount() {
-    // The JSON containing check data to populate the screenshot is uploaded
-    // to S3 by the Notificaption service (but any JSON URL would work)
+    // The JSON containing check notification data to populate the screenshot is
+    // uploaded to S3 by the Notificaption service (but any JSON URL would work)
     const jsonURI = this.props.location.query.json;
     this.props.actions.getCheckFromURI(jsonURI);
   },
-  getCheck() {
-    const checks = this.props.redux.checks.checks;
-    return checks.size ? checks.first() : new Map({id: this.props.params.id});
+  getNotification() {
+    const notification = this.props.redux.checks.notification;
+    const hasData = !!notification && !notification.isEmpty();
+    return hasData ? notification : new Map({ check_id: this.props.params.id });
   },
   getFailingResponses() {
-    const check = this.getCheck();
-    const responses = check.get('results').first().get('responses');
-
-    return responses.filter(response => !response.passing);
+    const notification = this.getNotification();
+    return notification.get('responses').filter(response => !response.passing);
+  },
+  isJSONLoaded() {
+    // True if the JSON has been loaded from the S3 URL
+    return this.getNotification().has('check_name');
   },
   renderInner() {
-    const check = this.getCheck();
-
-    if (check.get('name')) {
-      const spec = check.get('check_spec').value;
-      const target = check.get('target');
-
-      return (
-        <div className="js-screenshot-results">
-          <Padding b={1}>
-            <Heading level={3}>HTTP Request</Heading>
-            <HttpRequestItem spec={spec} target={target} />
-          </Padding>
-
-          <Padding b={1}>
-            <Heading level={3}>Assertions</Heading>
-            <AssertionItemList assertions={check.get('assertions')}/>
-          </Padding>
-
-          <Padding b={1}>
-            <CheckResponsePaginate responses={this.getFailingResponses().take(1)}
-              allowCollapse={false} />
-          </Padding>
-
-          <Padding b={1}>
-            <Heading level={3}>Notifications</Heading>
-            <NotificationItemList notifications={check.get('notifications')} />
-          </Padding>
-        </div>
-      );
+    if (!this.isJSONLoaded()) {
+      return null;
     }
 
-    return null;
+    return (
+      <div className="js-screenshot-results">
+        <CheckResponsePaginate responses={this.getFailingResponses().take(1)}
+          allowCollapse={false} />
+      </div>
+    );
   },
   render() {
-    const check = this.getCheck();
+    const check = this.getNotification();
 
     return (
       <div>
-        <Toolbar title={check.get('name') || check.get('id')} />
+        <Toolbar title={check.get('check_name') || check.get('check_id')} />
         <Grid>
           <Row>
             <Col xs={12}>
