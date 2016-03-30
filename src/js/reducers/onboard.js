d7fb872b7661af795c9ada2f2482f46e8930eba1
Corey Light <coreylight@gmail.com>
2016-03-30 15:50:46 -0500

    fix subnet selection

--- src/js/reducers/onboard.js
+++ src/js/reducers/onboard.js
@@ -59,7 +59,18 @@ function getFinalInstallData(state){
 
 function generateSubnetsForSelection(regions){
   return _.chain(regions).map(region => {
-    return _.chain(region.subnets).map(subnet => {
+
+    const selectedVpc = _.chain(region)
+    .get('vpcs')
+    .find('selected')
+    .get('vpc_id')
+    .value();
+
+    return _.chain(region.subnets)
+    .filter(subnet => {
+      return subnet.vpc_id === selectedVpc
+    })
+    .map(subnet => {
       let subnetName = subnet.subnet_id;
       if (subnet.tags.length){
         subnetName = _.chain(subnet.tags).find({key: 'Name'}).get('value').value() || subnetName;
@@ -70,6 +81,7 @@ function generateSubnetsForSelection(regions){
       <span style="color:${seed.color.text2}">${subnet.instance_count} Instances (${subnet.routing})</span>
       `];
     }).value();
+
   }).flatten().value();
 }
 
