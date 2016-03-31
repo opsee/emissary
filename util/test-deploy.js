5140f57be7d52976cace77b56682211375ee3264
Corey Light <coreylight@gmail.com>
2016-03-31 14:55:36 -0500

    mo test

--- util/test-deploy.js
+++ util/test-deploy.js
@@ -1,7 +1,8 @@
+var system = require('system');
+
 casper.options.verbose = true;
 casper.options.pageSettings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36";
 casper.test.begin('Ensure proper build', 1, function(test){
-  debugger;
   casper.start('http://localhost:8080/login', function(){
     this.on('page.error', function(msg, trace){
       this.echo('Error: '+msg, 'ERROR');
@@ -9,10 +10,20 @@ casper.test.begin('Ensure proper build', 1, function(test){
     this.on('remote.message', function(msg, trace){
       this.echo('From browser: '+msg);
     });
-    casper.waitForSelector('h1', function pass(){
-      test.pass('Found h1');
-    },
-    function fail(){
+    casper.waitForSelector('h1', function(){
+      console.log('found login h1');
+      casper.fill('form[name="loginForm"]', {
+        email: system.env.opseeEmail,
+        password: system.env.opseePassword
+      }, true);
+      casper.waitFor(function(){
+        return this.evaluate(function(){
+          return document.querySelector('h1').innerText === 'Checks';
+        });
+      }, function(){
+        test.pass('found checks page');
+      });
+    }, function(){
       test.fail('Could not find h1');
     }, 20000);
   });
