44459f0cf2dbc5ed71e1611add353af37e9119b5
Sara Bee <sara.bainbridge@gmail.com>
2016-04-11 17:44:49 -0400

    Add a couple of dorky axes

--- src/js/actions/env.js
+++ src/js/actions/env.js
@@ -272,6 +272,7 @@ export function getMetricRDS(id, metric) {
                             name
                             value
                             unit
+                            timestamp
                           }
                         }
                       }
--- src/js/components/graph/MetricGraph.js
+++ src/js/components/graph/MetricGraph.js
@@ -12,11 +12,48 @@ export default React.createClass({
   },
 
   render() {
-    console.log(this.props.data);
-    return (
-      <div>
-        graph
-      </div>
-    );
+    const node = ReactFauxDOM.createElement('svg');
+
+    // TODO responsiveness
+    const margin = {top: 20, right: 20, bottom: 30, left: 50}
+    const width = 960 - margin.left - margin.right
+    const height = 500 - margin.top - margin.bottom
+
+    const x = d3.time.scale()
+      .range([0, width]);
+
+    const y = d3.scale.linear()
+      .range([height, 0]);
+
+    const xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+    const yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+    const svg = d3.select(node)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis);
+
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Price ($)');
+
+    return node.toReact();
   }
 });
