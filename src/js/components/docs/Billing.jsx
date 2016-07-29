735cba580ef39782dcf6d30674b67bcf912d7ec3
Stephen Boak <stephen.boak@gmail.com>
2016-07-29 14:11:54 -0700

    changes and typos

--- src/js/components/docs/Billing.jsx
+++ src/js/components/docs/Billing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Link} from 'react-router';
+import {Link} from 'react-router';
 import {Toolbar} from '../global';
 import {Col, Grid, Panel, Row} from '../layout';
 import {Heading} from '../type';
@@ -19,11 +19,14 @@ const Billing = React.createClass({
                 <ol>
                   <li><strong>Free</strong>: the Free plan is for users with a single check. You do not need a credit card on file, and will not be charged anything to run a single check. You will not have access to some of our more advanced features, though, such as historical data and multi-user access to your account.</li>
                   <li><strong>Developer</strong>: Our Developer plan costs $5 per month for each check. If you have more than one check you are eligible for the Developer plan. All features apart from historical data and multi-user access are available at this level.</li>
-                  <li><strong>Team</strong>: Our Team plan costs $10 per month for each check. If you have more than one check you are eligible for the Developer plan. All Opsee features, including historical data and multi-user access, are available at this level.</li>
+                  <li><strong>Team</strong> (coming soon): Our Team plan costs $10 per month for each check. If you have more than one check you are eligible for the Team plan. All Opsee features, including historical data and multi-user access, are available at this level.</li>
                 </ol>
 
+                <Heading level={2}>Choosing and Changing Billing Plans</Heading>
+                <p>To choose or change a plan, head to your <Link to="/billing">Billing page</Link>. To turn off payments, choose the Free plan.</p>
+
                 <Heading level={2}>Trial</Heading>
-                <p>Every paid plan includes a 14-day free trial. The trial starts when you select the paid plan and enter a credit card number. You will not be billed until the end of your trial.</p>
+                <p>Every new customer receives a 14-day free trial, with access to all features and unlimited checks, that begins when you sign up. You will not be billed until the end of your trial.</p>
 
                 <Heading level={2}>Payments Processing and Credit Cards</Heading>
                 <p>Our payments are processed by Stripe, and Visa, MasterCard, American Express, JCB, Discover, and Diners Club cards are accepted.</p>
